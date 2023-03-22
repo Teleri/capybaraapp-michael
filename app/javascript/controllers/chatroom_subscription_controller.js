5aee8f864ca0213c26b95203a6e2905cc7ca8e59
Bruno Talarico <bruno.talarico@gmail.com>
2023-03-22 08:44:28 +0000

    chatroom

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
   static values = { chatroomId: Number, currentUserId: Number };
   static targets = ["messages"];
   connect() {
-    // createConsumer().subscriptions.create("ChatroomChannel");
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: (data) => this.#insertMessageAndScrollDown(data) }
@@ -16,8 +15,8 @@ export default class extends Controller {
   }
   #justifyClass(currentUserIsSender) {
     return currentUserIsSender
-      ? "justify-content-end"
-      : "justify-content-start";
+      ? "text-right"
+      : "text-left";
   }
   #userStyleClass(currentUserIsSender) {
     return currentUserIsSender ? "sender-style" : "receiver-style";
@@ -35,15 +34,13 @@ export default class extends Controller {
     `;
   }
   #insertMessageAndScrollDown(data) {
-    //Logic to know if the sender is the current_user
-    // const currentUserIsSender = this.currentUserIdValue === data.sender_id;
-    // // Creating the whole message from the `data.message` String
-    // const messageElement = this.#buildMessageElement(
-    //   currentUserIsSender,
-    //   data.message
-    // );
-    // Inserting the `message` in the DOM
-    this.messagesTarget.insertAdjacentHTML("beforeend", data);
+    const currentUserIsSender = this.currentUserIdValue === data.user;
+    const messageElement = this.#buildMessageElement(
+      currentUserIsSender,
+      data.message
+    );
+
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement);
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
   }
   resetForm(event) {
